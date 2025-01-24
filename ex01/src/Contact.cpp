f9fc903d3942622af1f11dc919dca1c3feb3974b
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-01-24 21:38:33 +0100

    get_info output format

--- ex01/src/Contact.cpp
+++ ex01/src/Contact.cpp
@@ -7,7 +7,7 @@ Contact::Contact()
     int i = -1;
     while (++i < 5)
     {
-        this->_info = {name, last_name, nickname, phone, secret}
+        this->_info
     }
 	return;
 }
@@ -20,13 +20,13 @@ Contact::~Contact(void)
 Contact::get_info(void)
 {
     bool is_big;
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 3; i++)
     {
+        std::cout << "|  " << i << "  ";
         is_big = this->_info[i].length() > 10;
         if (is_big)
-            std::cout << this->_info[i].substr(0,10) << ". |" << endl;
+            std::cout << "|  " << this->_info[i].substr(0,10) << ". | " << std::endl;
         else
-            // 
+            std::cout << "|  " << this->_info[i] << " | "; 
     }
-
 }
