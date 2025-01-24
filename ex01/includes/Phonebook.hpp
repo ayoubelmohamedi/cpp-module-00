04eb4bcaf99134967ab5f2cfe2da1d99b4706644
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-01-24 17:25:26 +0100

    phonebook header + class definition

---
+++ ex01/includes/Phonebook.hpp
@@ -0,0 +1,25 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP 
+
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <iostream>
+#include <Contact.hpp>
+
+class Phonebook
+{
+private:
+    Contact	_contacts[8];
+    int     _index;
+    
+
+public:
+    Phonebook();
+    ~Phonebook();
+
+    void add_user(Contact info);
+    void search();
+}
+
+#endif
