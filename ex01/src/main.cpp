98ca291a881edbfb9cc97de4e91b21e4daf5acca
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-01-27 18:40:10 +0100

    main is done

---
+++ ex01/src/main.cpp
@@ -0,0 +1,26 @@
+
+#include "Phonebook.hpp"
+
+int main(void)
+{
+    std::string input;
+    PhoneBook phonebook;
+
+    while (input != "EXIT")
+    {
+        std::cout << "Choose operation (ADD, SEARCH, EXIT):";
+        if (!std::getline(std::cin, input))
+        {
+            std::cerr << "Error reading input. Exiting..." << std::endl;
+            break;
+        }
+        if (input == "ADD")
+            phonebook.add_user();
+        else if (input ==  "SEARCH")
+            phonebook.search(); 
+        else if (input != "EXIT")
+            std::cout << "Invalid command. Please try again." << std::endl;
+    } 
+    return (0);
+}
+
