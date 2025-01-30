d59623f2924056edcbebf679126a76970aa6dcc3
ayoubelmohamedi <ayoubprog61@gmail.com>
2025-01-30 17:03:47 +0100

    reformat file

--- ex02/Account.hpp
+++ ex02/Account.hpp
@@ -29,14 +29,13 @@ public:
 	static int	getNbWithdrawals( void );
 	static void	displayAccountsInfos( void );
 
-	Account( int initial_deposit );
-	~Account( void );
+	Account(int initial_deposit);
+	~Account(void);
 
 	void	makeDeposit( int deposit );
 	bool	makeWithdrawal( int withdrawal );
 	int		checkAmount( void ) const;
 	void	displayStatus( void ) const;
-
 
 private:
 
